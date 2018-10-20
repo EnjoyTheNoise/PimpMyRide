bbb9dafe1ccf21a06bc62f0092c30708f839fbac
Unknown <jankos.pdp@gmail.com>
2018-10-20 22:32:46 +0200

    Added autofac module for data layer
    Added autofac to WebHost build process , addedd init configuration in startup (autofac, swagger, cors

--- PimpMyRide.Core/src/PimpMyRide.Core.Api/Program.cs
+++ PimpMyRide.Core/src/PimpMyRide.Core.Api/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace PimpMyRide.Core.Api
 {
@@ -20,6 +14,7 @@ namespace PimpMyRide.Core.Api
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureServices(services => services.AddAutofac())
                 .Build();
     }
 }
---
+++ PimpMyRide.Core/src/PimpMyRide.Core.Data/Infrastructure/RegisterModule/UnitOfWorkModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using PimpMyRide.Core.Data.UnitOfWork;
+
+namespace PimpMyRide.Core.Data.Infrastructure.RegisterModule
+{
+    public class UnitOfWorkModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UnitOfWork.UnitOfWork>().As<IUnitOfWork>().InstancePerLifetimeScope();
+        }
+    }
+}
