485f0c31d68fd4e627d17f88cb0bc19cafe7f610
Unknown <jankos.pdp@gmail.com>
2018-10-21 22:24:34 +0200

    Added API and service layer for getting/adding car
    Added Autofac modules in coresponding projects, set up DI

---
+++ PimpMyRide.Core/src/PimpMyRide.Core.Api/Infrastructure/RegisterModules/ApiModule.cs
@@ -0,0 +1,11 @@
+﻿using Autofac;
+
+namespace PimpMyRide.Core.Api.Infrastructure.RegisterModules
+{
+    public class ApiModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+        }
+    }
+}
