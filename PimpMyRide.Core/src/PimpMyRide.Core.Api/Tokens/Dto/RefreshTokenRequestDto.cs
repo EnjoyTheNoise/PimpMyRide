1b31d94ce74a6fee02c7e28205aa1fa2b9838b3f
Unknown <jankos.pdp@gmail.com>
2018-12-06 22:06:58 +0100

    Added TokenController with dtos
    TODO: move mappings to core

---
+++ PimpMyRide.Core/src/PimpMyRide.Core.Api/Tokens/Dto/RefreshTokenRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PimpMyRide.Core.Api.Tokens.Dto
+{
+    public class RefreshTokenRequestDto
+    {
+        [Required]
+        public string RefreshToken { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+    }
+}
