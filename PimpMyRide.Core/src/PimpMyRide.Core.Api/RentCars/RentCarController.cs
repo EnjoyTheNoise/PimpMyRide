36715cddba2a4f866ba05c44c44be39a13ff82ac
Bartosz97 <bartoszdudek5@wp.pl>
2018-12-09 12:31:16 +0100

    Added RentCarController

---
+++ PimpMyRide.Core/src/PimpMyRide.Core.Api/RentCars/RentCarController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PimpMyRide.Core.Api.Infrastructure;
+using PimpMyRide.Core.Cars.Dto;
+using PimpMyRide.Core.RentCars;
+using PimpMyRide.Core.RentCars.Dto;
+
+namespace PimpMyRide.Core.Api.RentCars
+{
+    [Route("api/rentcar")]
+    [Controller]
+    public class RentCarController : Controller
+    {
+        private readonly IRentCarService _rentCarService;
+
+        public RentCarController(IRentCarService rentCarService)
+        {
+            _rentCarService = rentCarService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(RentCarResponseDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> RentCar([FromBody] RentCarRequestDto dto, int id,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var result = await _rentCarService.RentCar(dto, id, cancellationToken);
+
+            if (result == null)
+            {
+                return BadRequest(new ApiResponse(StatusCodes.Status400BadRequest));
+            }
+
+            var response = new ApiOkResponse(result);
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(CancelRentCarResponseDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CancelRentCar(int id, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var result = await _rentCarService.CancelRentCar(id, cancellationToken);
+
+            if (result == null)
+            {
+                return BadRequest(new ApiResponse(StatusCodes.Status400BadRequest));
+            }
+
+            var response = new ApiOkResponse(result);
+            return Ok(response);
+        }
+    }
+}
