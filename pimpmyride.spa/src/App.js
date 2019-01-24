a7d871319e1d1270520a8ee57175f76350f23637
Unknown <jankos.pdp@gmail.com>
2019-01-24 17:52:47 +0100

    Added routing to application, added container for App component

--- pimpmyride.spa/src/App.js
+++ pimpmyride.spa/src/App.js
@@ -1,26 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarContainer from "./containers/NavbarContainer";
+import { Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
+import LoginFormContainer from "./containers/LoginFormContainer";
+import RegisterFormContainer from "./containers/RegisterFormContainer";
+import Home from "./components/Home";
+import CarsContainer from "./containers/CarsContainer";
 
 class App extends Component {
+  componentDidMount = () => {
+    if (localStorage.jwtToken) {
+      let token = {
+        securityToken: localStorage.getItem("jwtToken"),
+        refreshToken: localStorage.getItem("refreshToken"),
+        expiryDate: localStorage.getItem("Exp")
+      };
+      this.props.actions.setJwtToken(token);
+    } else {
+      this.props.actions.setJwtToken(false);
+    }
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="app">
+          <NavbarContainer />
+          <div className="container content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={LoginFormContainer} />
+              <Route path="/register" component={RegisterFormContainer} />
+              <Route path="/cars" component={CarsContainer} />
+            </Switch>
+          </div>
+          <ToastContainer />
+        </div>
+      </Router>
     );
   }
 }
